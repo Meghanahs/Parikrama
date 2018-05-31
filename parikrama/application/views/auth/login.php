b4e031113c912c3d20f72ae12f10469280fa19a4
Meghanahs <meghasrinivas122@gmail.com>
2018-06-01 00:21:03 +0530

    Sign Up page is added

--- parikrama/application/views/auth/login.php
+++ parikrama/application/views/auth/login.php
@@ -4,17 +4,15 @@
     <base href="<?php echo base_url();?>">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Rizvi">
-    <meta name="keyword" content="Php, Training, Coaching, Management, Software, Php, CodeIgniter, wake  Up, Accounting">
-    <link rel="shortcut icon" href="uploads/favicon.png">
 
     <title><?php echo lang('login'); ?> - <?php echo $this->db->get("settings")->row()->system_vendor; ?> </title>
 
     <!-- Bootstrap core CSS -->
+    
     <link href="common/css/bootstrap.min.css" rel="stylesheet">
     <link href="common/css/bootstrap-reset.css" rel="stylesheet">
     <!--external css-->
+    <link rel="stylesheet" href="common/css/tab.css">
     <link href="common/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
     <!-- Custom styles for this template -->
     <link href="common/css/style.css" rel="stylesheet">
@@ -29,19 +27,6 @@
 
   <body class="login-body">
 
-    <div class="container">
-
-      <form class="form-signin" method="post" action="auth/login">
-        <h2 class="login form-signin-heading"><?php echo $this->db->get('settings')->row()->login_title; ?><br/><br/><img alt="" width="100px" src="<?php echo $this->db->get('settings')->row()->login_logoo; ?>"></h2>
-        <div id="infoMessage"><?php echo $message;?></div>
-        <div class="login-wrap">
-            <input type="text" class="form-control" name="identity" placeholder="<?php echo lang('useremail'); ?>" autofocus>
-            <input type="password" class="form-control" name="password" placeholder="<?php echo lang('password'); ?>">
-            
-            
-           
-           
-           <p><a data-toggle="modal" href="#myModal"> <?php echo lang('forgot_password'); ?></a></p>
       <!--     
             <label class="checkbox">
                 <input type="checkbox" value="remember-me"> Remember me
@@ -51,27 +36,54 @@
                 </span>
             </label> 
       -->
-            <button class="btn btn-lg btn-login btn-block" type="submit"> <?php echo lang('sign_in'); ?></button>
-            
-          
-
+      
+<div class="row">
+<div class="col-md-4 col-md-offset-4">
+<div class="form-body">
+    <ul class="nav nav-tabs final-login">
+        <li class="active"><a data-toggle="tab" href="#sectionA">Sign In</a></li>
+        <li><a data-toggle="tab" href="#sectionB">Sign Up</a></li>
+    </ul>
+    <div class="tab-content">
+        <div id="sectionA" class="tab-pane fade in active">
+        <div class="innter-form">
+        <form class="sa-innate-form" method="post" action="auth/login">
+        <h2 class="login form-signin-heading">Parikrama</h2>
+      <div id="infoMessage"><?php echo $message;?></div>
+            <label>Email Address</label>
+             <input type="text" class="form-control" name="identity" placeholder="<?php echo lang('useremail'); ?>" autofocus>          
+            <label>Password</label>
+             <input type="password" class="form-control" name="password" placeholder="<?php echo lang('password'); ?>">
+          <center>   <button type="submit" >Sign In</button></center>
+            <p style="text-align:center;"><a data-toggle="modal" href="#myModal"> <?php echo lang('forgot_password'); ?></a></p>
+        <!--    </button> -->
+            </form>
+            </div>
+           
+            <div class="clearfix"></div>
         </div>
-
-        
-
-      </form>
-
+        <div id="sectionB" class="tab-pane fade">
+			<div class="innter-form">
+            <form class="sa-innate-form" method="post">
+			  <div id="infoMessage"><?php echo $message;?></div>
+            <label>Name</label>
+            <input type="text" name="username" autofocus>
+            <label>Email Address</label>
+            <input type="text" name="username">
+			<label>Phone Number</label>
+            <input type="text" name="phone_no">
+            <label>Password</label>
+            <input type="password" name="password">
+           <center> <button type="submit">Register</button></center>
+            </form>
+            </div>
+        </div>
     </div>
-      
-      
-      
-      
-      
-      
-      
-        
-        
-          <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+    </div>
+    </div>
+    </div>
+    
+     <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
               <div class="modal-dialog">
                   <div class="modal-content">
                       <form method="post" action="auth/forgot_password">
@@ -95,8 +107,8 @@
           </div>
           
 
-
     <!-- js placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="common/js/jquery-1.11.1.min.js"></script>
     <script src="common/js/jquery.js"></script>
     <script src="common/js/bootstrap.min.js"></script>
 
